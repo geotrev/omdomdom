949f706bef3d2644c387ab5780ca44cbb7db1b05
George Treviranus <3946669+geotrev@users.noreply.github.com>
2020-09-17 12:00:34 -0700

    Update omdomdom.js

--- src/omdomdom.js
+++ src/omdomdom.js
@@ -1,6 +1,6 @@
-import { forEach } from "./utilities"
+import { forEach, createKeyMap, isnertBefore, patch } from "./utilities"
 import { toHTML } from "./parsers"
-import { getAttributes } from "./attributes"
+import { updateAttributes, getAttributes } from "./attributes"
 
 /**
  * Object representation of a DOM element.
@@ -13,6 +13,107 @@ import { getAttributes } from "./attributes"
  * @property {HTMLElement} node - The corresponding DOM element.
  * @property {boolean} isSVGContext
  */
+
+/**
+ * Both template and vNode have arrays of virtual nodes. Diff them.
+ * @param {VirtualNode} template - new virtual node tree.
+ * @param {VirtualNode} vNode - existing virtual node tree.
+ */
+const updateChildren = (template, vNode) => {
+  const templateChildrenLength = template.children.length
+
+  if (!templateChildrenLength && !vNode.children.length) return
+
+  const [vNodeKeyMap, hasKeys] = createKeyMap(vNode.children)
+
+  // There were no keys found:
+
+  if (!hasKeys) {
+    // Remove extra nodes if template.children is smaller
+    let delta = vNode.children.length - templateChildrenLength
+    if (delta > 0) {
+      while (delta-- > 0) {
+        const child = vNode.children.pop()
+        vNode.node.removeChild(child.node)
+      }
+    }
+
+    return forEach(template.children, (templateChild, idx) => {
+      const vNodeChild = vNode.children[idx]
+
+      if (typeof vNodeChild !== "undefined") {
+        update(templateChild, vNodeChild)
+      } else {
+        vNode.node.appendChild(templateChild.node)
+        vNode.children.push(templateChild)
+      }
+    })
+  }
+
+  // There were keys found, resolve them:
+
+  let nextChildren = Array(templateChildrenLength)
+
+  // Match keys and update/move children in-place
+  forEach(template.children, (child, idx) => {
+    const childNodes = vNode.node.childNodes
+    const key = child.attributes.key
+
+    if (Object.prototype.hasOwnProperty.call(vNodeKeyMap, key)) {
+      const keyChild = vNodeKeyMap[key]
+
+      if (Array.prototype.indexOf.call(childNodes, keyChild.node) !== idx) {
+        insertBefore(vNode, keyChild, childNodes[idx])
+      }
+
+      nextChildren[idx] = keyChild
+
+      // Prevent duplicates, remove the entry and let it insert at
+      // its natural index in the `else` block.
+      delete vNodeKeyMap[key]
+      update(child, nextChildren[idx])
+    } else {
+      insertBefore(vNode, child, childNodes[idx])
+      nextChildren[idx] = child
+    }
+  })
+
+  vNode.children = nextChildren
+
+  // Remove any real nodes that are left over from the diff
+  let childNodesLength = vNode.node.childNodes.length
+  let delta = childNodesLength - templateChildrenLength
+  if (delta > 0) {
+    while (delta-- > 0) {
+      vNode.node.removeChild(vNode.node.childNodes[childNodesLength - 1])
+      childNodesLength--
+    }
+  }
+}
+
+/**
+ * Reconcile differences between two virtual DOM trees.
+ * @param {VirtualNode} template - new virtual node tree.
+ * @param {VirtualNode} vNode - existing virtual node tree.
+ * @param {Node} rootNode - the HTML element containing the current node context
+ */
+export const update = (template, vNode, rootNode = vNode.node.parentNode) => {
+  // Node nodes to compare, exit
+  if (!template && !vNode) return
+  const contentChanged = template.content && template.content !== vNode.content
+
+  // If the type or content changed, replace the node completely
+  if (template.type !== vNode.type || contentChanged) {
+    rootNode.replaceChild(template.node, vNode.node)
+    return patch(template, vNode)
+  }
+
+  // Update attributes, if any
+  updateAttributes(template, vNode)
+
+  // Diff child nodes recursively
+  updateChildren(template, vNode)
+}
 
 /**
  * Renders a node into the given root context. This happens one time,
